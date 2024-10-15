4dee98ec815774a775ee318dd8eff53d9be74765
Leonid-spec <winneraftersales@gmail.com>
2024-10-15 23:12:39 +0200

    Initial commit

--- src/vertex/Lesson_18.java
+++ src/vertex/Lesson_18.java
@@ -3,7 +3,7 @@ package vertex;
 import java.util.Scanner;
 
 public class Lesson_18 {
-    //еобходимо вывести на экран числа от 1 до 5. На экране должно быть:
+    //необходимо вывести на экран числа от 1 до 5. На экране должно быть:
     public static void main(String[] args) {
   //      for (int i = 1; i <= 10; i++){
   //          System.out.println("3 * " + i + " = " + 3*i);
